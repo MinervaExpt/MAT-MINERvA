fc6d7498cc571efd5763eb2a3b928dee33c95df5
Andrew Olivier <andrew@djoinc.com>
2021-07-07 15:49:20 -0500

    Purged classes that we don't need dictionaries for anymore.

--- dict/MAT-MINERvADict.h
+++ dict/MAT-MINERvADict.h
@@ -18,25 +18,6 @@
 
 #include <vector>
 
-/*#include "NSFDefaults.h"
-#include "AnaBinning.h"
-#include "ArachneUtils.h"
-#include "Exceptions.h"
-#include "HistogramUtils.h"
-#include "MnvFluxConstraint.h" 
-#include "MnvAnaTuple.h"
-#include "MnvPlotter.h"
-#include "MnvRecoShifter.h"
-#include "TargetUtils.h"
-#include "MnvNormalization.h"
-#include "POTCounter.h"
-#include "FluxReweighter.h"
-#include "FluxReweighterWithWiggleFit.h"
-#include "HyperDimLinearizer.h"
-#include "PhysicsVariables.h"
-#include "MnvColors.h"
-#include "GridCanvas.h"*/
-
 // PlotUtils weight classes
 #include "weighters/weightRPA.h"
 #include "weighters/weight_2p2h.h"
@@ -65,34 +46,8 @@
 // see: http://root.cern.ch/root/roottalk/roottalk10/0035.html
 // somehow std::map<>s seem to be instantiated somewhere else, so explicit instantiation is not necessary?
 #ifdef __GCCXML__
-/*template class std::vector<PlotUtils::MnvEVD::Event>;                                       // the 'Events' typedef
-template class std::pair<std::string, std::vector<PlotUtils::MnvEVD::Event> >;              // the 'EventGroup' typedef
-
-template class std::map<std::string, std::vector<std::string> >;
-template class std::pair<std::string, std::vector<std::string> >;*/
-
-// The std::pair<>s for those std::map<>s don't seem to be generated though.
-/*template class std::pair< std::string, PlotUtils::MnvLatErrorBand* >;
-template class std::pair< std::string, PlotUtils::MnvVertErrorBand* >;
-template class std::pair< std::string, TMatrixT<double>* >;
-template class std::pair< std::string, PlotUtils::MnvVertErrorBand2D* >;
-template class std::pair< std::string, PlotUtils::MnvLatErrorBand2D* >;
-template class std::pair< std::string, PlotUtils::MnvVertErrorBand3D* >;
-template class std::pair< std::string, PlotUtils::MnvLatErrorBand3D* >;*/
 
 // Use extern keyword because these functions are instantiated in FluxReweighter.cxx already
-/*extern template void PlotUtils::FluxReweighter::AddFluxErrorBand<PlotUtils::MnvH1D>(PlotUtils::MnvH1D*);
-extern template void PlotUtils::FluxReweighter::AddFluxErrorBand<PlotUtils::MnvH2D>(PlotUtils::MnvH2D*);
-extern  template MnvH1D* FluxReweighter::GetIntegratedFluxReweighted<MnvH1D>( int nuPDG,
-                                                                 MnvH1D* template_hist,
-                                                                 double min_energy,
-                                                                 double max_energy,
-                                                                 bool use_muon_correlations);
-extern template MnvH2D* FluxReweighter::GetIntegratedFluxReweighted<MnvH2D>( int nuPDG,
-                                                                 MnvH2D* template_hist,
-                                                                 double min_energy,
-                                                                 double max_energy,
-                                                                 bool use_muon_correlations);*/
 
 #endif
 #endif // PLOTUTILSDICT_H
